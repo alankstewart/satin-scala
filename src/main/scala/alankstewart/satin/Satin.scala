948404c5d2e1ff507c7364c8836863098dc012fc
Alan Stewart <alankstewart@gmail.com>
2013-11-20 20:39:10 +1100

    Changed indentation from 4 to 2

--- src/main/scala/alankstewart/satin/Satin.scala
+++ src/main/scala/alankstewart/satin/Satin.scala
@@ -5,51 +5,51 @@ import com.typesafe.config._
 
 object Satin {
 
-    object CO2 extends Enumeration {
-        val MD, PI = Value
-    }
+  object CO2 extends Enumeration {
+    val MD, PI = Value
+  }
 
-    class Laser(val outputFile: String, val smallSignalGain: Float, val dischargePressure: Int,
-                val carbonDioxide: CO2.Value) {
-        override def toString() = outputFile + "  " + smallSignalGain + "  " + dischargePressure + "  " + carbonDioxide
-    }
+  class Laser(val outputFile: String, val smallSignalGain: Float, val dischargePressure: Int,
+    val carbonDioxide: CO2.Value) {
+    override def toString() = outputFile + "  " + smallSignalGain + "  " + dischargePressure + "  " + carbonDioxide
+  }
 
-    def main(args: Array[String]) {
-        println("Hello, world!")
-        calculate
-    }
+  def main(args: Array[String]) {
+    println("Hello, world!")
+    calculate
+  }
 
-    def calculate() {
-        getInputPowers.foreach(println)
-        getLaserData.foreach(println)
-        // for (laser <- getLaserData) {
-        //     println(laser)
-        // }
-        println(getOutputFilePath)
+  def calculate() {
+    getInputPowers.foreach(println)
+    getLaserData.foreach(println)
+    for (laser <- getLaserData) {
+      println(laser)
     }
+    println(getOutputFilePath)
+  }
 
-    def getInputPowers: List[Int] = {
-        val source = Source.fromFile(getDataFilePath + "pin.dat")
-        val inputPowers = source.getLines.map(line => line.trim.toInt).toList
-        source.close()
-        return inputPowers
-    }
+  def getInputPowers: List[Int] = {
+    val source = Source.fromFile(getDataFilePath + "pin.dat")
+    val inputPowers = source.getLines.map(line => line.trim.toInt).toList
+    source.close()
+    return inputPowers
+  }
 
-    def getLaserData: List[Laser] = {
-        val source = Source.fromFile(getDataFilePath + "laser.dat")
-        val laserData = source.getLines.map(line => {
-            val tokens = line.split("  ")
-            new Laser(tokens(0), tokens(1).trim.toFloat, tokens(2).trim.toInt, CO2.withName(tokens(3).trim))
-        }).toList
-        source.close
-        return laserData
-    }
+  def getLaserData: List[Laser] = {
+    val source = Source.fromFile(getDataFilePath + "laser.dat")
+    val laserData = source.getLines.map(line => {
+      val tokens = line.split("  ")
+      new Laser(tokens(0), tokens(1).trim.toFloat, tokens(2).trim.toInt, CO2.withName(tokens(3).trim))
+    }).toList
+    source.close
+    return laserData
+  }
 
-    def getDataFilePath: String = {
-        ConfigFactory.load().getString("dataFilePath")
-    }
+  def getDataFilePath: String = {
+    ConfigFactory.load().getString("dataFilePath")
+  }
 
-    def getOutputFilePath: String = {
-        ConfigFactory.load().getString("outputFilePath")
-    }
+  def getOutputFilePath: String = {
+    ConfigFactory.load().getString("outputFilePath")
+  }
 }
