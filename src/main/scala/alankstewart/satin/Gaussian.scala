4aba51362586cb666669f520d83e35ddbbb791f0
Alan Stewart <alankstewart@gmail.com>
2013-11-25 13:55:31 +1100

    Create Gaussian.scala
    Major refactor

---
+++ src/main/scala/alankstewart/satin/Gaussian.scala
@@ -0,0 +1,16 @@
+package alankstewart.satin
+
+import scala.math.log
+import scala.math.BigDecimal._
+import scala.math.BigDecimal.RoundingMode.HALF_UP
+
+class Gaussian(val inputPower: Int, val outputPower: Double, val saturationIntensity: Int) {
+
+  def logOutputPowerDividedByInputPower(): BigDecimal = {
+    double2bigDecimal(log(outputPower / inputPower)).setScale(3, HALF_UP)
+  }
+
+  def outputPowerMinusInputPower(): BigDecimal = {
+    (double2bigDecimal(outputPower) - int2bigDecimal(inputPower)).setScale(3, HALF_UP)
+  }
+}
