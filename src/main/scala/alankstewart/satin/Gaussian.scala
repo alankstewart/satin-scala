3a4262600260b2ed71199216fb0aeac994e65202
Alan Stewart <alankstewart@gmail.com>
2013-11-29 10:36:26 +1100

    Update Gaussian.scala
    Changed indent to recommended 2

--- src/main/scala/alankstewart/satin/Gaussian.scala
+++ src/main/scala/alankstewart/satin/Gaussian.scala
@@ -6,11 +6,11 @@ import scala.math.BigDecimal.RoundingMode.HALF_UP
 
 class Gaussian(val inputPower: Int, val outputPower: Double, val saturationIntensity: Int) {
 
-    def logOutputPowerDividedByInputPower(): BigDecimal = {
-        double2bigDecimal(log(outputPower / inputPower)).setScale(3, HALF_UP)
-    }
+  def logOutputPowerDividedByInputPower(): BigDecimal = {
+    double2bigDecimal(log(outputPower / inputPower)).setScale(3, HALF_UP)
+  }
 
-    def outputPowerMinusInputPower(): BigDecimal = {
-        (double2bigDecimal(outputPower) - int2bigDecimal(inputPower)).setScale(3, HALF_UP)
-    }
+  def outputPowerMinusInputPower(): BigDecimal = {
+    (double2bigDecimal(outputPower) - int2bigDecimal(inputPower)).setScale(3, HALF_UP)
+  }
 }
